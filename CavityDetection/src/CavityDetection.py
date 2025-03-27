8f563e486a8f9666a8edeb35395a0167132cc966
hu4nghe <he.hu4ng@outlook.com>
2025-03-27 17:11:32 +0100

    Fisrt Usable Version

--- CavityDetection/src/CavityDetection.py
+++ CavityDetection/src/CavityDetection.py
@@ -1,20 +1,45 @@
 import cv2
 import numpy as np
 
-image = cv2.imread("CavityDetection/Connectors/50T20.png")
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+def DetecteCavity(Img) :
+    HSV = cv2.cvtColor(Img, cv2.COLOR_BGR2HSV) # Convert to HSV for Mask 
 
-blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+    LowerBlack = np.array([0, 0, 0])
+    UpperBlack = np.array([180, 255, 30])  # Only dark Area
+    Mask = cv2.inRange(HSV, LowerBlack, UpperBlack)
 
-# Hough Circle Transform
-circles = cv2.HoughCircles(blurred, cv2.HOUGH_GRADIENT, dp=1.2, minDist=30,
-                           param1=50, param2=33, minRadius=15, maxRadius=50)
+    # Denoising
+    Ker = np.ones((3,3), np.uint8)
+    Mask = cv2.morphologyEx(Mask, cv2.MORPH_OPEN, Ker, iterations=2)
 
-if circles is not None:
-    circles = np.uint16(np.around(circles))
-    for i in circles[0, :]:
-        cv2.circle(image, (i[0], i[1]), i[2], (0, 255, 0), 2)
+    # Find Contours
+    Contours, _ = cv2.findContours(Mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-cv2.imshow("Detected Pins", image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+
+    Cavities = []
+
+    for DetectedCnt in Contours:
+       Area = cv2.contourArea(DetectedCnt)
+       (x,y), R = cv2.minEnclosingCircle(DetectedCnt)
+    
+       # Calculate circularity
+       Perimeter = cv2.arcLength(DetectedCnt, True)
+       if Perimeter == 0:
+           continue
+       Circularity = 4 * np.pi * Area / (Perimeter * Perimeter)
+    
+
+       if (R > 10 and               
+           R < 50 and              
+           Circularity > 0.7 and        
+           Area > 30):     
+           
+        Centre = (int(x), int(y))
+        R = int(R)
+        Cavities.append((Centre, R))
+        
+        #draw
+        cv2.circle(Img, Centre, R, (0,255,0), 2)
+    
+    return Img, Cavities
+        
