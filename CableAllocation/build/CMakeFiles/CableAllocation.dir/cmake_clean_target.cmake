a14915dfefe2aa314f543d64b7b7697a97d326a9
hu4nghe <he.hu4ng@outlook.com>
2025-03-28 12:36:46 +0100

    remove build dictories

--- CableAllocation/build/CMakeFiles/CableAllocation.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "/home/hhg10/project/CableAllocation/lib/libCableAllocation.a"
-)
--- CableAllocation/build/CMakeFiles/CableAllocation.dir/src/CableAllocation.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/CableAllocation.dir/src/CableAllocation.cpp.o: \
- /home/hhg10/project/CableAllocation/src/CableAllocation.cpp \
- /usr/include/stdc-predef.h
