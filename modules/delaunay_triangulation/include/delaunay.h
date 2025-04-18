45c3eac1c48e7854bc7a97118a472a8dddd0d0e8
HUANG He <he.huang.intern@3ds.com>
2025-04-18 11:24:16 +0200

    add delaunay triangulation header

---
+++ modules/delaunay_triangulation/include/delaunay.h
@@ -0,0 +1,15 @@
+/**
+ * @file delaunay.h
+ * @author HUANG He (he.huang.intern@3ds.com)
+ * @brief 
+ * Delaunay triangulation header file.
+ * @version 1.0
+ * @date 2025-04-18 
+ * @copyright 
+ * Dassault Systemes 2025
+ */
+#include <vector>
+#include <tuple>
+#include <unordered_map>
+
+std::unordered_map<int, std::vector<int>> delaunay_triangulation(const std::vector<std::tuple<int, int, double, double>>& input_points);
